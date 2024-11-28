4330f62cf01f8cab3d26452318d0769e743108ec
Thomas Degallaix <t.degallaix@niryo.com>
2024-11-28 10:55:45 +0100

    Confirm packages installation automatically

--- deploy_bridge.sh
+++ deploy_bridge.sh
@@ -24,7 +24,7 @@ usage()
 install_docker() {
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -36,7 +36,7 @@ install_docker() {
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 install() {
